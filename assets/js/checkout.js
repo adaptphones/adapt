69f0f36147d46d54b2ef5439fd28425da102ff41
lindensheehy <linden.sheehy@outlook.com>
2023-11-26 15:11:26 -0500

    shop page looks much better

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,12 +1,88 @@
+const decodeDict = {
+    '7' : 'a',
+    'n' : 'b',
+    'p' : 'c',
+    '.' : 'd',
+    '6' : 'e',
+    'u' : 'f',
+    'V' : 'g',
+    'x' : 'h',
+    'H' : 'i',
+    'C' : 'j',
+    'R' : 'k',
+    '1' : 'l',
+    'P' : 'm',
+    'd' : 'n',
+    'g' : 'o',
+    'k' : 'p',
+    'L' : 'q',
+    '4' : 'r',
+    'q' : 's',
+    'S' : 't',
+    '8' : 'u',
+    'v' : 'v',
+    'T' : 'w',
+    'e' : 'x',
+    'Z' : 'y',
+    'f' : 'z',
+    'y' : 'A',
+    'Q' : 'B',
+    'l' : 'C',
+    'J' : 'D',
+    '9' : 'E',
+    '_' : 'F',
+    'i' : 'G',
+    'o' : 'H',
+    't' : 'I',
+    'h' : 'J',
+    'E' : 'K',
+    'W' : 'L',
+    'X' : 'M',
+    '0' : 'N',
+    'G' : 'O',
+    'z' : 'P',
+    'b' : 'Q',
+    '-' : 'R',
+    'a' : 'S',
+    'D' : 'T',
+    'j' : 'U',
+    'Y' : 'V',
+    'U' : 'W',
+    'r' : 'X',
+    's' : 'Y',
+    'm' : 'Z',
+    '5' : '1',
+    'I' : '2',
+    'w' : '3',
+    '2' : '4',
+    'c' : '5',
+    'N' : '6',
+    'K' : '7',
+    'A' : '8',
+    '3' : '9',
+    'O' : '0',
+    'M' : '-',
+    'F' : '_',
+    'B' : '.',
+};
+
+function decode(data) {
+    let retString = "";
+    for (let i = 0; i < data.length; i++) {
+        retString = retString.concat(decodeDict[data[i]]);
+    }
+    return retString
+}
+
 // Fetches the product which was stored on the shop page now that the user is on the checkout page
 document.addEventListener('DOMContentLoaded', function () {
     
     // Get stored string
-    const product = sessionStorage.getItem('product');
+    const urlParams = new URLSearchParams(window.location.search);
+    const product = decode(urlParams.get('product'));
 
     // Find the elements which will be changed
     const productName = document.getElementById('productName');
-    productName.innerHTML = product;
 
     // Load the page based on the product
     switch (product) {
@@ -20,22 +96,22 @@ document.addEventListener('DOMContentLoaded', function () {
             productName.innerHTML = "40 MP Camera";
             break;
         case 'product4':
-            productName.innerHTML = "10 MP Camera";
+            productName.innerHTML = "product4";
             break;
         case 'product5':
-            productName.innerHTML = "20 MP Camera";
+            productName.innerHTML = "product5";
             break;
         case 'product6':
-            productName.innerHTML = "40 MP Camera";
+            productName.innerHTML = "product6";
             break;
         case 'product7':
-            productName.innerHTML = "10 MP Camera";
+            productName.innerHTML = "product7";
             break;
         case 'product8':
-            productName.innerHTML = "20 MP Camera";
+            productName.innerHTML = "product8";
             break;
         case 'product9':
-            productName.innerHTML = "40 MP Camera";
+            productName.innerHTML = "product9";
             break;
     }
 });
--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,7 +1,162 @@
+const encodeDict = {
+    'a' : '7',
+    'b' : 'n',
+    'c' : 'p',
+    'd' : '.',
+    'e' : '6',
+    'f' : 'u',
+    'g' : 'V',
+    'h' : 'x',
+    'i' : 'H',
+    'j' : 'C',
+    'k' : 'R',
+    'l' : '1',
+    'm' : 'P',
+    'n' : 'd',
+    'o' : 'g',
+    'p' : 'k',
+    'q' : 'L',
+    'r' : '4',
+    's' : 'q',
+    't' : 'S',
+    'u' : '8',
+    'v' : 'v',
+    'w' : 'T',
+    'x' : 'e',
+    'y' : 'Z',
+    'z' : 'f',
+    'A' : 'y',
+    'B' : 'Q',
+    'C' : 'l',
+    'D' : 'J',
+    'E' : '9',
+    'F' : '_',
+    'G' : 'i',
+    'H' : 'o',
+    'I' : 't',
+    'J' : 'h',
+    'K' : 'E',
+    'L' : 'W',
+    'M' : 'X',
+    'N' : '0',
+    'O' : 'G',
+    'P' : 'z',
+    'Q' : 'b',
+    'R' : '-',
+    'S' : 'a',
+    'T' : 'D',
+    'U' : 'j',
+    'V' : 'Y',
+    'W' : 'U',
+    'X' : 'r',
+    'Y' : 's',
+    'Z' : 'm',
+    '1' : '5',
+    '2' : 'I',
+    '3' : 'w',
+    '4' : '2',
+    '5' : 'c',
+    '6' : 'N',
+    '7' : 'K',
+    '8' : 'A',
+    '9' : '3',
+    '0' : 'O',
+    '-' : 'M',
+    '_' : 'F',
+    '.' : 'B',
+};
+
+function encode(data) {
+    let retString = "";
+    for (let i = 0; i < data.length; i++) {
+        retString = retString.concat(encodeDict[data[i]]);
+    }
+    return retString
+}
+
 // Runs from the shop page to store the product which the user clicked
 function checkout(product) {
 
     // Give the URL a parameter with the product name and load the checkout page
-    const encodedData = encodeURIComponent(product);
+    const encodedData = encodeURIComponent(encode(product));
     window.location.href = 'productcheckout.html?product=' + encodedData;
+}
+
+function getTitleOpacity(scroll) {
+    if (scroll > 800) return 0;
+    return 1 - (scroll / 800);
+}
+
+function getArrowOpacity(scroll) {
+    if (scroll > 400) return 0;
+    return 1 - (scroll / 400);
+}
+
+function getTitleOffset(scroll) {
+    if (scroll > 800) return "100px";
+
+    let base = 100;
+
+    base += (1 - (scroll / 800)) * 200;
+
+    return base.toString().concat("px");
+}
+
+function getArrowOffset(scroll) {
+    if (scroll > 400) return "100px";
+
+    let offset = (scroll / 400) * 100;
+
+    return offset.toString().concat("px");
+}
+
+// Makes the Title text and the down arrows fade out when the user scrolls
+document.addEventListener('DOMContentLoaded', function () {
+    const pageTitle = document.getElementById('shopTitle');
+    const arrow1 = document.getElementById('arrow1');
+    const arrow2 = document.getElementById('arrow2');
+    const arrow3 = document.getElementById('arrow3');
+
+    window.addEventListener('scroll', function () {
+        const currentScrollPosition = window.scrollY;
+
+        // Get opacity and vertical offset values for title and arrows
+        let titleOpacity = getTitleOpacity(currentScrollPosition);
+        let titleOffset = getTitleOffset(currentScrollPosition);
+        let arrowOpacity = getArrowOpacity(currentScrollPosition);
+        let arrowOffset = getArrowOffset(currentScrollPosition);
+
+        // Set the style values
+        pageTitle.style.opacity = titleOpacity;
+        pageTitle.style.top = titleOffset;
+        arrow1.style.opacity = arrowOpacity;
+        arrow1.style.top = arrowOffset;
+        arrow2.style.opacity = arrowOpacity;
+        arrow2.style.top = arrowOffset;
+        arrow3.style.opacity = arrowOpacity;
+        arrow3.style.top = arrowOffset;
+    });
+});
+
+function nothing() {
+    if (currentScrollPosition != lastScrollPosition) {
+
+        let titleOpacity = getTitleOpacity(currentScrollPosition);
+        let titleOffset = getTitleOffset(currentScrollPosition);
+
+        let arrowOpacity = getArrowOpacity(currentScrollPosition);
+        let arrowOffset = getArrowOffset(currentScrollPosition);
+
+        pageTitle.innerHTML = currentScrollPosition;
+            
+        // Move and fade out title and arrows
+        pageTitle.style.opacity = titleOpacity;
+        pageTitle.style.top = titleOffset;
+        // arrow1.style.opacity = arrowOpacity;
+        // arrow1.style.top = arrowOffset;
+        // arrow2.style.opacity = arrowOpacity;
+        // arrow2.style.top = arrowOffset;
+        // arrow3.style.opacity = arrowOpacity;
+        // arrow3.style.top = arrowOffset;
+    }
 }
